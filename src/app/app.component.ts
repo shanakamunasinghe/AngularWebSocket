4ebd363116a79227d6eb54b1b55e45ed9c568f6c
Shanaka <shanakamunasingheplay@gmail.com>
2020-06-01 18:56:09 +0530

    make web socket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {WebSocketAPI} from './WebSocketAPI';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-websocket';
+  title = 'angular8-springboot-websocket';
+
+  webSocketAPI: WebSocketAPI;
+  greeting: any;
+  name: string;
+
+  ngOnInit() {
+    this.webSocketAPI = new WebSocketAPI(new AppComponent());
+  }
+
+  connect() {
+    this.webSocketAPI._connect();
+  }
+
+  disconnect() {
+    this.webSocketAPI._disconnect();
+  }
+
+  sendMessage() {
+    this.webSocketAPI._send(this.name);
+  }
+
+  handleMessage(message) {
+    this.greeting = message;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
